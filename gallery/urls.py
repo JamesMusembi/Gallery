b5b407eb478a614d6a2cfd81049a35f4f9526db0
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-28 19:14:21 +0300

    creating url

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name="indexpage"),
-    path('search/', views.show_category, name="search_categories"),
-    path('oneimage/<int:pk>', views.viewPhoto, name="viewPhoto"),
-    path('searchbylocation/', views.show_by_location, name="showlocation")
+    # path('search/', views.show_category, name="search_categories"),
+    # path('oneimage/<int:pk>', views.viewPhoto, name="viewPhoto"),
+    # path('searchbylocation/', views.show_by_location, name="showlocation")
 ]
